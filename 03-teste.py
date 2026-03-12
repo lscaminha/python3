5671673091ea5d3bb8caebc693d8b18f144cabba
Luciana Caminha <lscaminha@gmail.com>
2026-03-12 12:39:07 -0300

    Primeiros scripts em python

---
+++ 03-teste.py
@@ -0,0 +1,7 @@
+ano_atual = int(input("Digite o ano atual:"))
+ano_nasc = int(input("Digite o ano do seu nascimento:"))
+
+idade = int(ano_atual - ano_nasc)
+
+print()
+print("Sua idade é:", idade, "anos")
---
+++ scripts/teste.py
@@ -0,0 +1,8 @@
+base = int(input("Digite o valor da base: "))
+largura = int(input("Digite o valor de largura: "))
+
+area = base * largura
+
+print("A area do retângulo é:", area, "unidades quadradas")
+
+
